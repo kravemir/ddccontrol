bf5bdb14e648bfb26d4052a33ec26042c99a1596
Miroslav Kravec <kravec.miroslav@gmail.com>
2018-05-24 18:59:52 +0200

    Ignore glib leaks in valgrind

--- scripts/valgrind_dbus_service_check.sh
+++ scripts/valgrind_dbus_service_check.sh
@@ -1,26 +1,39 @@
 #!/usr/bin/env bash
 
+set -e
 make -j4
+
+DIRECTORY=`dirname $0`
+
+if [ ! -d ${DIRECTORY}/tmp/GNOME.supp ]; then
+    mkdir -p ${DIRECTORY}/tmp;
+    (cd ${DIRECTORY}/tmp; git clone https://github.com/dtrebbien/GNOME.supp.git);
+fi
+
+(cd ${DIRECTORY}/tmp/GNOME.supp; make)
 
 [ `whoami` = root ] || exec sudo su -c $0 root
 
 echo 'make install; remove dbus service'
 make install
-find /usr/{,local/}share/dbus* -name "ddccontrol.DDCControl.service" -exec rm '{}' \;
+
+[ -d /usr/share/dbus-1 ] && find /usr/share/dbus* -name "ddccontrol.DDCControl.service" -exec rm '{}' \;
+[ -d /usr/local/share/dbus-1 ] && find /usr/local/share/dbus* -name "ddccontrol.DDCControl.service" -exec rm '{}' \;
 
 VALGRIND_OUT=$(mktemp /tmp/ddccontrol_service.valgrind.out.XXXXXXXX)
 chmod 755 "${VALGRIND_OUT}"
 
 echo "kill all ddccontrol & ddcpci processes"
-pkill ddccontrol
-pkill ddcpci
+pkill ddccontrol || true
+pkill ddcpci || true
 
 sleep 0.25
 
-pgrep ddc
+pgrep ddc || true
 
-echo "starting service"
-su -c "libtool --mode=execute valgrind --leak-check=full --log-file='${VALGRIND_OUT}' '$(pwd)/src/daemon/ddccontrol_service'" &
+CCMD="libtool --mode=execute valgrind --suppressions='$(pwd)'/scripts/tmp/GNOME.supp/build/{base,gio,glib}.supp --leak-check=full --log-file='${VALGRIND_OUT}' '$(pwd)/src/daemon/ddccontrol_service'"
+echo "starting service: $CCMD"
+su -c "$CCMD" &
 DDCCONTROL_DBUS_SERVICE_PID=$!
 
 sleep 5
@@ -28,6 +41,6 @@ sleep 5
 echo "run tests"
 "$(pwd)/src/ddccontrol/ddccontrol" -p >/dev/null
 
-kill ${DDCCONTROL_DBUS_SERVICE_PID}
+kill -15 ${DDCCONTROL_DBUS_SERVICE_PID}
 
 echo "valgrind output written to: ${VALGRIND_OUT}"
